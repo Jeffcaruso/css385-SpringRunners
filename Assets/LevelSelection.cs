0449d0e5276a41ab8a826a1e6ce06b34b6a7d125
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-05-22 17:56:21 -0700

    Updated Level Selection Screen
    Switched to button control.
    Added background for something interesting.
    Added our hero to also add something interesting and relevant to the scene

--- Assets/LevelSelection.cs
+++ Assets/LevelSelection.cs
@@ -16,53 +16,66 @@ public class LevelSelection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
-
-        if (Input.GetKeyDown("0"))
-        {
-            //level Tutorial (malachi)
-            SceneManager.LoadScene("malachi tutorial");
-        }
-        else if (Input.GetKeyDown("1"))
-        {
-            //tutorial (level 1)
-            SceneManager.LoadScene("Tutorial-Scene");
-        }
-        else if (Input.GetKeyDown("2"))
-        {
-            //level 2 (Jeff)
-            SceneManager.LoadScene("Jeffrey-Scene");
-        }
-        else if (Input.GetKeyDown("3"))
-        {
-            //level 3 (lawrence)
-            SceneManager.LoadScene("Lawrence-Scene");
-        }
-        else if (Input.GetKeyDown("4"))
-        {
-            //level 4 (Malachi)
-            SceneManager.LoadScene("Malachi-Scene");
-        }
-        else if (Input.GetKeyDown("5"))
-        {
-            //level 5 (Joseph)
-            SceneManager.LoadScene("Joseph-Scene");
-        }
+        //if (Input.GetKeyDown("0"))
+        //{
+        //    //level Tutorial (malachi)
+        //    SceneManager.LoadScene("malachi tutorial");
+        //}
+        //else if (Input.GetKeyDown("1"))
+        //{
+        //    //tutorial (level 1)
+        //    SceneManager.LoadScene("Tutorial-Scene");
+        //}
+        //else if (Input.GetKeyDown("2"))
+        //{
+        //    //level 2 (Jeff)
+        //    SceneManager.LoadScene("Jeffrey-Scene");
+        //}
+        //else if (Input.GetKeyDown("3"))
+        //{
+        //    //level 3 (lawrence)
+        //    SceneManager.LoadScene("Lawrence-Scene");
+        //}
+        //else if (Input.GetKeyDown("4"))
+        //{
+        //    //level 4 (Malachi)
+        //    SceneManager.LoadScene("Malachi-Scene");
+        //}
+        //else if (Input.GetKeyDown("5"))
+        //{
+        //    //level 5 (Joseph)
+        //    SceneManager.LoadScene("Joseph-Scene");
+        //}
         
-        else if (Input.GetKeyDown("6")){
-            SceneManager.LoadScene("Boss Level");
-        }
+        //else if (Input.GetKeyDown("6")){
+        //    SceneManager.LoadScene("Boss Level");
+        //}
 
-        //insert more levels here as needed...
+        ////insert more levels here as needed...
 
-        else
-        {
-            //do nothing
-        }
+        //else
+        //{
+        //    //do nothing
+        //}
     }
 
     public void TutorialLoad()
+    {
+        SceneManager.LoadScene("Malachi tutorial");
+    }
+    public void Level1Load()
+    {
+        SceneManager.LoadScene("Jeffrey-Scene");
+    }
+    public void Level2Load()
+    {
+        SceneManager.LoadScene("Lawrence-Scene");
+    }
+    public void Level3Load()
+    {
+        SceneManager.LoadScene("Malachi-Scene");
+    }
+    public void BossLevelLoad()
     {
         SceneManager.LoadScene("Boss Level");
     }
